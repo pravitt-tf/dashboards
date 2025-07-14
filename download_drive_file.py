4e62d0d1a42723bf20ca615c5830ed52b04ae256
Pravitt Sethi <21f2001055@ds.study.iitm.ac.in>
2025-07-14 17:02:44 +0530

    py file

---
+++ download_drive_file.py
@@ -0,0 +1,26 @@
+import os
+import io
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+
+FILE_ID = os.environ["HTML_FILE_ID"]
+DEST_FILE = "public/index.html"
+
+creds = service_account.Credentials.from_service_account_info(
+    eval(os.environ["GCP_SA_KEY"]),
+    scopes=["https://www.googleapis.com/auth/drive.readonly"]
+)
+
+drive = build("drive", "v3", credentials=creds)
+
+request = drive.files().get_media(fileId=FILE_ID)
+fh = io.FileIO(DEST_FILE, "wb")
+downloader = MediaIoBaseDownload(fh, request)
+
+done = False
+while not done:
+    status, done = downloader.next_chunk()
+    print(f"Downloading... {int(status.progress() * 100)}%")
+
+print("✅ File downloaded to public/index.html")
