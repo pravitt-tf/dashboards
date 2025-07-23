7f62a33c860364eb504f2fbc26603ef526378dc0
Pravitt Sethi <21f2001055@ds.study.iitm.ac.in>
2025-07-23 17:57:16 +0530

    added a dashboard and edited key generator

--- key_generator.py
+++ key_generator.py
@@ -2,7 +2,7 @@ import random
 import string
 
 def generate_key(length=10):
-    characters = string.ascii_letters + string.digits
+    characters = string.ascii_lowercase + string.digits
     return ''.join(random.choices(characters, k=length))
 
 if __name__ == "__main__":
